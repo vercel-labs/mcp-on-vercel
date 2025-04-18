e4a7ef1e31210e80324c7624eea1f9e28c2d12ed
Malte Ubl <malte.ubl@gmail.com>
2025-04-18 05:48:55 -0700

    external

--- lib/mcp-api-handler.ts
+++ lib/mcp-api-handler.ts
@@ -44,6 +44,9 @@ export function initializeMcpApiHandler(
   let servers: McpServer[] = [];
 
   let statelessServer: McpServer;
+  const statelessTransport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: undefined,
+  });
 
   return async function mcpApiHandler(
     req: IncomingMessage,
@@ -53,9 +56,6 @@ export function initializeMcpApiHandler(
     const url = new URL(req.url || "", "https://example.com");
     if (url.pathname === "/mcp") {
       console.log("Got new MCP connection", req.url, req.method);
-      const statelessTransport = new StreamableHTTPServerTransport({
-        sessionIdGenerator: undefined,
-      });
 
       if (!statelessServer) {
         statelessServer = new McpServer(
@@ -67,8 +67,8 @@ export function initializeMcpApiHandler(
         );
 
         initializeServer(statelessServer);
+        await statelessServer.connect(statelessTransport);
       }
-      await statelessServer.connect(statelessTransport);
       await statelessTransport.handleRequest(req, res);
     } else if (url.pathname === "/sse") {
       console.log("Got new SSE connection");
